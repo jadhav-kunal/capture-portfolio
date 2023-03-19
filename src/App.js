f71ac11d5bfd343fc3ea04cbfb6b96163d01bfa7
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-03-19 20:53:58 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import GlobalStyle from "./components/GlobalStyle";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
