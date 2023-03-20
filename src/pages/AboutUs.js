15bda18e23a7d4a12de27b8baf96b0a30be2248d
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-03-20 22:14:31 +0530

    Started with framer motion and added page transitions

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,24 @@
 import React from "react";
-// Page Components
+//Page Components
 import AboutSection from "../components/AboutSection";
-import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,20 @@
 import React from "react";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      style={{ background: "#fff" }}
+    >
+      <h1>Our CONTACT</h1>
+    </motion.div>
+  );
 };
 
 export default ContactUs;
